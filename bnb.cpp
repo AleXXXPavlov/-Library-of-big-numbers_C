d07599e6d754c2465cefbddffeb06859134d8681
tOpDeveLOpeR-pAs <71667213+tOpDeveLOpeR-pAs@users.noreply.github.com>
2020-10-22 01:09:22 +0300

    Add files via upload

---
+++ bnb.cpp
@@ -0,0 +1,256 @@
+﻿
+#include "bnb.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+int NOTATION = 10; // система счисления, с которой записаны числа в массив
+
+//int Change_Sign(bn*); // функция для смены знака
+
+/*int Change_Sign(bn* Obj) {
+	if (Obj == NULL) 
+	{
+		return BN_NULL_OBJECT;
+	}
+
+	Obj->sign == 1 ? Obj->sign = -1 : Obj ->sign = 1;
+	return BN_OK;
+}*/
+
+
+/* Определения структуры bn и ее функций */
+struct bn_s {
+	int* ptr_body; // указатель на начало массива цифр
+	size_t size; // размер массива
+	int sign; // знак числа
+};
+
+/* Конструктор */
+bn* bn_new() {
+	bn* ptr_bn = (bn*)malloc(sizeof(bn));
+
+	if (ptr_bn == NULL) {
+		return NULL;
+	}
+
+	ptr_bn->size = 1;
+	ptr_bn->sign = 0;
+	ptr_bn->ptr_body = calloc(ptr_bn->size, sizeof(int));
+
+	if (ptr_bn->ptr_body == NULL) {
+		free(ptr_bn);
+		return NULL;
+	}
+	return ptr_bn;
+}
+
+/* Конструктор копирования */
+bn* bn_init(const bn *Obj) {
+	if (Obj == NULL) {
+		return NULL;
+	}
+
+	bn* ptr_cbn = calloc(sizeof(bn), sizeof(int));
+	if (ptr_cbn == NULL) {
+		return NULL;
+	}
+
+	ptr_cbn->size = Obj->size;
+	ptr_cbn->sign = Obj->sign;
+
+	ptr_cbn->ptr_body = calloc(ptr_cbn->size, sizeof(int));
+	if (ptr_cbn->ptr_body == NULL) {
+		free(ptr_cbn);
+		return NULL;
+	}
+
+	memcpy(ptr_cbn->ptr_body, Obj->ptr_body, Obj->size);
+	return ptr_cbn;
+}
+
+/* Деструктор */
+int bn_delete(bn* Obj) {
+	if (Obj == NULL) {
+		return BN_NULL_OBJECT;
+	}
+
+	if (Obj->ptr_body != NULL)
+		free(Obj->ptr_body);
+
+	free(Obj);
+
+	return BN_OK;
+}
+
+/* Инициализация числа представлением строки в 10-й системе счисления */
+int bn_init_string(bn* Obj, const char* str) {
+	if (Obj == NULL) {
+		return BN_NULL_OBJECT;
+	}
+
+	size_t length = strlen(str);
+	size_t i = 0;
+
+	Obj->sign = 1;
+	if (str[0] == '-') {
+		Obj->sign = -1;
+		++i;
+	}
+	
+	Obj->size = length - i;
+
+	int* arr_num = (int*)calloc(Obj->size, sizeof(int));
+	if (arr_num != NULL) {
+		for (size_t j = 0; j < Obj->size; ++j) {
+			arr_num[j] = str[length - j - 1] & 0x0F;
+		}
+	}
+
+	Obj->ptr_body = arr_num;
+
+	return 0;
+}
+
+/* Инициализация числа представлением строки в некоторой системе счисления */
+int bn_init_string_radix(bn* Obj, const char* str, int radix) {
+	if (Obj == NULL) {
+		return BN_NULL_OBJECT;
+	}
+
+	return BN_OK;
+}
+
+/* Функция для прибавления одного большого числа к другому */
+int bn_add_to(bn* Obj1, const bn* Obj2) {
+	if (Obj1 == NULL || Obj2 == NULL) {
+		return BN_NULL_OBJECT;
+	}
+
+	// Проверки на равенство нулю
+	if (Obj2->sign == 0) {
+		Obj1 = bn_init(Obj2);
+		return BN_OK;
+	}
+	if (Obj2->sign == 0) {
+		return BN_OK;
+	}
+
+	// Сравнение знаков
+	if (Obj1->ptr_body != NULL && Obj2->ptr_body != NULL)
+	{
+		if (Obj1->sign == Obj2->sign) // случай, когда оба знака совпадают
+		{
+			/* Добавление куска памяти с 0, для сравнивания размеров */
+			if (Obj1->size < Obj2->size) {
+				Obj1->ptr_body = (int*)realloc(Obj1->ptr_body, Obj2->size);
+				if (Obj1->ptr_body == NULL) {
+					return BN_NO_MEMORY;
+				}
+
+				for (size_t j = Obj1->size; j < Obj2->size; ++j) {
+					Obj1->ptr_body[j] = 0;
+				}
+				Obj1->size = Obj2->size;
+			}
+
+			size_t i = 0;
+			int flag = 0; // параметр, сигнализирующий, о получении слишком большого числа в ячейке
+
+			for (; i < Obj2->size; ++i) {
+				Obj1->ptr_body[i] += flag + Obj2->ptr_body[i];
+				
+				flag = (Obj1->ptr_body[i] >= NOTATION); 
+				if (flag != 0)
+				{
+					Obj1->ptr_body[i] -= NOTATION;
+				}
+			}
+
+			if (flag != 0)
+			{
+				while (flag != 0 && i < Obj1->size)
+				{
+					Obj1->ptr_body[i - 1] -= NOTATION;
+					Obj1->ptr_body[i] += flag;
+
+					flag = (Obj1->ptr_body[i] >= NOTATION);
+					if (flag != 0)
+					{
+						Obj1->ptr_body[i] -= NOTATION;
+						Obj1->ptr_body[i] += 1;
+					}
+
+					++i;
+				}
+
+				if (flag != 0)
+				{
+					Obj1->ptr_body = (int*)realloc(Obj1->ptr_body, 1);
+					if (Obj1->ptr_body == NULL)
+					{
+						return BN_NO_MEMORY;
+					}
+					++(Obj1->size);
+
+					Obj1->ptr_body[Obj1->size - 1] = flag;
+				}
+			}
+			
+		}
+		else // случай, когда у чисел разные знаки 
+		{
+			bn* Obj_c = bn_new();
+			Obj_c = bn_init(Obj2); // временная копия Obj2 со знаком +
+			//Change_Sign(Obj_c); // смена знака
+			Obj_c->sign = -(Obj_c->sign);
+
+			int code =  bn_sub_to(Obj1, Obj_c);
+			bn_delete(Obj_c);
+			return code;
+		}
+	}
+	else {
+		return BN_NO_MEMORY;
+	}
+
+	return BN_OK;
+}
+
+int bn_sub_to(bn* Obj1, const bn* Obj2) {
+	return BN_OK;
+}
+
+/* Функция для вывода данных о большом числе */
+int bn_print(bn const* Obj)
+{
+	if (Obj == NULL)
+	{
+		return BN_NULL_OBJECT;
+	}
+	printf("\nSign = %d\nLength = %d\nAbsolute value = ", Obj->sign, Obj->size);
+	
+	for (int i = Obj->size - 1; i != -1; --i) {
+		printf("%d", Obj->ptr_body[i]);
+	}
+	printf("\n");
+
+	return BN_OK;
+}
+
+int main() {
+	bn *bn1 = bn_new();
+	bn_init_string(bn1, "4754384385748");
+
+	bn *bn2 = bn_new();
+	bn_init_string(bn2, "4573465934473984575976598643789585428645784263575957235853858351375882567589086");
+	
+	bn* bn0 = bn_new();
+
+	int result = bn_add_to(bn1, bn2);
+	bn_print(bn1);
+
+	printf("\n%d\n", result);
+	return 0;
+}
---
+++ bnb.h
@@ -0,0 +1,62 @@
+#ifndef bnb
+#define bnb
+
+struct bn_s;
+typedef struct bn_s bn;
+
+enum bn_codes {
+	BN_OK, BN_NULL_OBJECT, BN_NO_MEMORY, BN_DIVIDE_BY_ZERO
+};
+
+bn* bn_new(); // Создать новое BN
+bn* bn_init(const bn*); // Создать копию существующего BN
+
+// Инициализировать значение BN десятичным представлением строки
+int bn_init_string(bn*, const char*);
+
+// Инициализировать значение BN представлением строки
+// в системе счисления: radix
+int bn_init_string_radix(bn*, const char*, int);
+
+// Инициализировать значение BN заданным целым числом
+int bn_init_int(bn*, int);
+
+// Уничтожить BN (освободить память)
+int bn_delete(bn*);
+
+// Операции, аналогичные +=, -=, *=, /=, %=
+int bn_add_to(bn*, bn const*);
+int bn_sub_to(bn*, bn const*);
+int bn_mul_to(bn*, bn const*);
+int bn_div_to(bn*, bn const*);
+int bn_mod_to(bn*, bn const*);
+
+// Возвести число в степень degree
+int bn_pow_to(bn*, int);
+
+// Извлечь корень степени reciprocal из BN
+int bn_root_to(bn*, int);
+
+// Аналоги операций x = l+r (l-r, l*r, l/r, l%r)
+bn* bn_add(bn const*, bn const*);
+bn* bn_sub(bn const*, bn const*);
+bn* bn_mul(bn const*, bn const*);
+bn* bn_div(bn const*, bn const*);
+bn* bn_mod(bn const*, bn const*);
+
+// Выдать представление BN в системе счислени: radix в виде строки
+// Строку после использования потребуется удалить.
+const char* bn_to_string(bn const*, int);
+
+// Если левое меньше, вернуть <0; если равны, вернуть 0; иначе >0
+int bn_cmp(bn const*, bn const*);
+
+int bn_neg(bn*); // Изменить знак на противоположный
+int bn_abs(bn*); // Вернуть модуль
+int bn_sign(bn const*); //-1 если t<0; 0 если t = 0, 1 если t>0
+
+
+
+
+
+#endif bnb
